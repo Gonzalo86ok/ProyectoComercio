9a274b119a9b5a455463f3b37674d0e17ea7fc7f
Gonzalo <gonzalopasos87@gmail.com>
2023-10-31 09:35:47 -0300

    agregue el listado de fabricantes

--- ProyectoComercio/Comercio/ProveedorPage.aspx.cs
+++ ProyectoComercio/Comercio/ProveedorPage.aspx.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Dominio;
+using Negocio;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,6 +13,14 @@ namespace Comercio
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                FabricanteNegocio fabricanteNegocio = new FabricanteNegocio();                         
+                List<Fabricante> fabricantes = fabricanteNegocio.listar();
+                 
+                RepeaterFabricantes.DataSource = fabricantes;
+                RepeaterFabricantes.DataBind();             
+            }
 
         }
     }
